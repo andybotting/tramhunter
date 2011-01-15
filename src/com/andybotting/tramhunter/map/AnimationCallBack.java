1ad2f51d19e4a92c0f1b0d123e00a5c4dcd1e118
Andy Botting <andy@andybotting.com>
2011-01-15 11:23:33 +0000

    Fix conflict

--- src/com/andybotting/tramhunter/map/AnimationCallBack.java
+++
@@ -1,5 +0,0 @@
-package com.andybotting.tramhunter.map;
-
-abstract class AnimationCallBack {
-	abstract void onTimer(int centerX, int centerY, float scale);
-}
--- src/com/andybotting/tramhunter/map/SizeCallBack.java
+++
@@ -1,5 +0,0 @@
-package com.andybotting.tramhunter.map;
-
-abstract class SizeCallBack {
-	abstract void onSizeChanged(int w, int h);
-}
