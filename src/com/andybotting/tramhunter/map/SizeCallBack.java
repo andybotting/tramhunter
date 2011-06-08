40b349148570fe8afd524df09876bd5a0a4a3ec8
Andy Botting <andy@andybotting.com>
2011-06-08 15:56:33 +0000

    Include license header on all java and xml files

--- src/com/andybotting/tramhunter/map/SizeCallBack.java
+++ src/com/andybotting/tramhunter/map/SizeCallBack.java
@@ -1,3 +1,13 @@
+/* 
+ * Copyright AndroidBigImage
+ * 
+ * http://code.google.com/p/androidbigimage/
+ *   
+ * The code below is licenced under Creative Commons 3.0 BY-SA
+ * http://creativecommons.org/licenses/by-sa/3.0/
+ * 
+ */
+
 package com.andybotting.tramhunter.map;
 
 abstract class SizeCallBack {
