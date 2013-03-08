c13ef8390aaae9c66c1173fb96bea118119bcddc
Andy Botting <andy@andybotting.com>
2013-03-08 22:35:17 +1100

    Update constants in prep for location rework

--- android/TramHunter/src/com/andybotting/tramhunter/TramHunterConstants.java
+++ android/TramHunter/src/com/andybotting/tramhunter/TramHunterConstants.java
@@ -1,5 +1,5 @@
 /*  
- * Copyright 2012 Andy Botting <andy@andybotting.com>
+ * Copyright 2013 Andy Botting <andy@andybotting.com>
  *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,35 +34,33 @@
 
 package com.andybotting.tramhunter;
 
-import android.app.AlarmManager;
-
 public class TramHunterConstants {
+
+	/**
+	 * These values control the user experience of your app. You should
+	 */
+	// The minimum time between location updates
+	public static int MIN_TIME = 1000 * 20;
+
+	// The minimum distance between location updates
+	public static int MIN_DISTANCE = 50;
 	
-  /**
-   * These values control the user experience of your app. You should
-   */
-  // The default search radius when searching for places nearby.
-  public static int DEFAULT_RADIUS = 150;
-  
-  // The maximum distance the user should travel between location updates. 
-  public static int MAX_DISTANCE = DEFAULT_RADIUS/2;
-  
-  // The maximum time that should pass before the user gets a location update.
-  public static long MAX_TIME = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
-  
-  // The maximum number of nearby stops to show in list.
-  public static int MAX_NEARBY_STOPS = 30;
+	// Time delta between comparing location updates
+	public static int LOCATION_UPDATE_DELTA = 1000 * 30;
 
-  /**
-   * These values are constants used for intents, exteas, and shared preferences.
-   * You shouldn't need to modify them.
-   */
-  public static boolean SUPPORTS_JELLYBEAN = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN;
-  public static boolean SUPPORTS_ICS = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH;
-  public static boolean SUPPORTS_GINGERBREAD = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD;
-  public static boolean SUPPORTS_HONEYCOMB = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB;
-  public static boolean SUPPORTS_FROYO = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.FROYO;
+	// The maximum number of nearby stops to show in list.
+	public static int MAX_NEARBY_STOPS = 30;
 
+	/**
+	 * These values are constants used for intents, exteas, and shared
+	 * preferences. You shouldn't need to modify them.
+	 */
+	public static boolean SUPPORTS_JELLYBEAN = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN;
+	public static boolean SUPPORTS_ICS = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ICE_CREAM_SANDWICH;
+	public static boolean SUPPORTS_GINGERBREAD = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.GINGERBREAD;
+	public static boolean SUPPORTS_HONEYCOMB = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.HONEYCOMB;
+	public static boolean SUPPORTS_FROYO = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.FROYO;
+	public static boolean SUPPORTS_ECLAIR = android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.ECLAIR_MR1;
 }
 
 
