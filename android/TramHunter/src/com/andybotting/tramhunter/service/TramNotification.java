865d5ce7721e23f943492a07d8688ebd0e9183d0
Andy Botting <andy@andybotting.com>
2013-07-30 20:24:48 +1000

    Update GPL notices, and bump date to 2013

--- android/TramHunter/src/com/andybotting/tramhunter/service/TramNotification.java
+++ android/TramHunter/src/com/andybotting/tramhunter/service/TramNotification.java
@@ -1,6 +1,6 @@
 /*  
  * Copyright 2012 Tarcio Saraiva <tarcio@gmail.com> 
- * Copyright 2012 Andy Botting <andy@andybotting.com> 
+ * Copyright 2013 Andy Botting <andy@andybotting.com> 
  *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
