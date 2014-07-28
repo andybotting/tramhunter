727f416bcd2bb1b27fde3dad6071e5a5e0974a02
Andy Botting <andy@andybotting.com>
2014-07-28 14:17:47 +1000

    Update to v1.4

--- android/TramHunter/src/com/andybotting/tramhunter/dao/TramHunterDB.java
+++ android/TramHunter/src/com/andybotting/tramhunter/dao/TramHunterDB.java
@@ -72,7 +72,7 @@ public class TramHunterDB extends SQLiteOpenHelper {
 	// E.g.
 	// App Version v0.2.92 = DB Version 292
 	// App Version v1.2.0 = DB Version 1200
-	private static final int DATABASE_VERSION = 1391;
+	private static final int DATABASE_VERSION = 1400;
 
 	private SQLiteDatabase mDB = null;
 	private Context mContext;
